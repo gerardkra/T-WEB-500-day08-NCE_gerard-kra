b1f77077e3c27dfd022229ff58c4ef7d9d6634cb
Gérard Kra <150908185+gerardkra@users.noreply.github.com>
2025-10-01 11:57:52 +0200

    Update task04.php

--- task04.php
+++ task04.php
@@ -8,12 +8,12 @@ function calc(string $operator, int $nbr1, int $nbr2) {
         case "*":
             return $nbr1 * $nbr2;
         case "/":
-            if ($nbr2 == 0) {
+            if ($nbr2 === 0) {
                 return "Cannot divide by 0";
             }
             return $nbr1 / $nbr2;
-        case "%": // nbr1%nbr2 means the rest value of the division of nbr1 by nbr2, so nbr2==0 result in impossible division.
-            if ($nbr2 == 0) {
+        case "%":
+            if ($nbr2 === 0) {
                 return "Cannot divide by 0";
             }
             return $nbr1 % $nbr2;
@@ -21,5 +21,3 @@ function calc(string $operator, int $nbr1, int $nbr2) {
             return "Unknown operator";
     }
 }
-?>
-
